b5fd4600e9509fe1dcd55b6903d8ca0e0f593067
EmadNova <im.nova@yahoo.com>
2023-07-30 16:49:40 +0330

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Head from "next/head";
+
+const Page = () => {
+    return (
+        <div>
+          welcome to landing
+        </div>
+    );
+};
+
+export default Page;
