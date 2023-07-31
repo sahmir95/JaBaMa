9d18042a977acb4621060b2f70f7fb1d6c723061
EmadNova <im.nova@yahoo.com>
2023-07-31 19:35:39 +0330

    feat: 320 scale header landing done

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import HeaderLanding from "@/components/header/HeaderLanding";
+import HeaderPop from "@/components/header/headerPop/HeaderPop";
 
 const Page = () => {
     return (
         <div>
-          welcome to landing
+            <HeaderPop/>
+            <HeaderLanding/>
         </div>
     );
 };
