bfe964babdbb3367b015412dfe522bf09d84250d
sahmir95 <sahar2200048@gmail.com>
2023-08-01 23:30:46 +0330

    feat(page.jsx add)

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import HeaderLanding from "@/components/header/HeaderLanding";
+import HeaderPop from "@/components/header/headerPop/HeaderPop";
+import Menu from "@/components/menu/Menu";
 
 const Page = () => {
     return (
         <div>
-
+            <HeaderPop/>
+            <HeaderLanding/>
         </div>
     );
 };
