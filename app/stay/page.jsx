939031b4fa54a43e9ef7a84ac24e5e713e89fe20
Amirhossein Hemmati <amirhemmati15@yahoo.com>
2023-08-02 11:44:28 +0330

    feat:create router od each accomodation

---
+++ app/stay/page.jsx
@@ -0,0 +1,5 @@
+
+
+export default function Stay() {
+    return <h1>About Page</h1>;
+}
--- components/imageComponent/Image.jsx
+++ components/imageComponent/Image.jsx
@@ -1,8 +1,26 @@
-
+import {useState,useEffect} from "react";
+import data from "../../json/data.json"
 export const Image = () => {
+    const [jsonData,setJsonData]=useState(null)
+    useEffect(() => {
+        setJsonData(data)
+    },[]);
+
     return (
         <div>
-            <div></div>
+                {jsonData.map((id,images) => (
+                <div>
+                    <div key={id}>
+                        <img src={images[0]} alt="image-one"/>
+                    </div>
+                    <div>
+                        <img src={images[1]} alt="image-two"/>
+                        <img src={images[2]} alt="image-three"/>
+                        <img src={images[3]} alt="image-four"/>
+                        <img src={images[4]} alt="image-five"/>
+                    </div>
+                </div>
+                ))}
         </div>
     );
 };
