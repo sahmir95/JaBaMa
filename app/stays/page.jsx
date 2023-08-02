c9724ecb3d692b8b1edc9ad1f9ee84a1c5c61d42
Masencod <sajjad.ghorashy@gmail.com>
2023-08-02 19:56:34 +0330

    added searchOptions component

--- app/stays/page.jsx
+++ app/stays/page.jsx
@@ -6,21 +6,27 @@ export default async function page( ) {
 
     const data = await getLocalData()
 
+
+    
+
     return (
       <layout>
-        <div className="pt-2 pb-3">
-            <div className="pr-3 pl-5 h-11 flex items-center">
+        <div className="pt-2 pb-3 border-b border-main-border-gray">
+            <div className="pr-3 mb-3 pl-5 h-11 flex items-center">
                 <div className="px-2 flex justify-center items-center">
                     <Link href="/" >
                         <img className="h-3" src="/images/right-arrow.svg" alt="arrow" />
                     </Link>
                 </div>
-                <div>
+                <div className="">
                     {/* <Search />   */}
                     <div>
                         <span>search</span>
                     </div>
                 </div>
+            </div>
+            <div>
+
             </div>
         </div>
         <div>
---
+++ components/SearchOptions.jsx
@@ -0,0 +1,43 @@
+
+
+export default SearchOptions = () => {
+  const filterFields = [
+    {
+      name: "انتخاب تاریخ",
+      icon: '',
+      onClick: () => console.log("are")
+    },
+    {
+      name: "تعداد نفرات",
+      icon: '',
+      onClick: () => console.log("are")
+    },
+    {
+      name: "فیلترها",
+      icon: '',
+      onClick: () => console.log("are")
+    },  
+    {
+      name: "انتخاب شهر",
+      icon: '',
+      onClick: () => console.log("are")
+    },
+    {
+      name: "قیمت برای هر شب",
+      icon: '',
+      onClick: () => console.log("are")
+    },         
+    {
+      name: "نوع اقامتگاه",
+      icon: '',
+      onClick: () => console.log("are")
+    },  
+    
+  ]
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
