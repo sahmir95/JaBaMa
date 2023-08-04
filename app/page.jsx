8f9e9efa10fc81b6ac3453c228d87e8a0ade4252
EmadNova <im.nova@yahoo.com>
2023-08-04 18:18:26 +0330

    feat: background image added, z-index added to header

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Page = () => {
     return (
      <div>
-
+         <section className="relative">
+             <div className="shadowHeader">
+                 <img className="w-full h-full imageRendering object-cover" src="/images/background.jpg" alt="bg"/>
+             </div>
+         </section>
      </div>
     );
 };
