81de5bf339f5fb3d8c05e2974c6f3fee049dc919
EmadNova <im.nova@yahoo.com>
2023-08-05 21:21:14 +0330

    fix: landingpage fix some issues

--- app/page.jsx
+++ app/page.jsx
@@ -14,40 +14,42 @@ const data = await getLocalData();
 
 const Page = () => {
   return (
-    <div className="w-full min-h-screen flex flex-col items-center">
-      <LandingPage />
-      <AutoSwiper />
-      <IconsSection />
-      <div className="w-full">
-        <Favplc data={data.cities} title={"محبوب ترین مقصد‌ها"} />
+      <div>
+          <LandingPage />
+          <div className="w-full min-h-screen flex flex-col items-center">
+              <AutoSwiper />
+              <IconsSection />
+              <div className="w-full">
+                  <Favplc data={data.cities} title={"محبوب ترین مقصد‌ها"} />
+              </div>
+              <BoxWithSwiper
+                  data={data.data[0].vila}
+                  city={data.cities[2].province}
+                  title="جذاب ترین ویلاها در جاباما"
+                  subtitle="ویلاهای استخردار واسه سفر آخر هفته"
+              >
+                  <CardWithSwiper isCocacity={false} />
+              </BoxWithSwiper>
+              <BoxWithSwiper
+                  data={data.data[1].hotel}
+                  city={data.cities[2].province}
+                  title="بهترین هتل ها با جاباما"
+                  subtitle="اقامتِ باکیفت در بهترین هتل"
+              >
+                  <CardWithSwiper isCocacity={false} />
+              </BoxWithSwiper>
+              <BoxWithSwiper
+                  data={data.data[3].cottage}
+                  city={data.cities[2].province}
+                  title="کلبه‌های خاص"
+                  subtitle="کلبه‌هلی مثلثی و چوبی"
+              >
+                  <CardWithSwiper isCocacity={false} />
+              </BoxWithSwiper>
+              <LastSection />
+              <TagSection data={data.data[0].vila} />
+          </div>
       </div>
-      <BoxWithSwiper
-        data={data.data[0].vila}
-        city={data.cities[2].province}
-        title="جذاب ترین ویلاها در جاباما"
-        subtitle="ویلاهای استخردار واسه سفر آخر هفته"
-      >
-        <CardWithSwiper isCocacity={false} />
-      </BoxWithSwiper>
-      <BoxWithSwiper
-        data={data.data[1].hotel}
-        city={data.cities[2].province}
-        title="بهترین هتل ها با جاباما"
-        subtitle="اقامتِ باکیفت در بهترین هتل"
-      >
-        <CardWithSwiper isCocacity={false} />
-      </BoxWithSwiper>
-      <BoxWithSwiper
-        data={data.data[3].cottage}
-        city={data.cities[2].province}
-        title="کلبه‌های خاص"
-        subtitle="کلبه‌هلی مثلثی و چوبی"
-      >
-        <CardWithSwiper isCocacity={false} />
-      </BoxWithSwiper>
-      <LastSection />
-      <TagSection data={data.data[0].vila} />
-    </div>
   );
 };
 
