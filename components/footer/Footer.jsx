ec66be1728498385c84f20e49564cfb95a96f4b4
EmadNova <im.nova@yahoo.com>
2023-08-07 15:59:32 +0330

    feat: 404 responsive and 404 large scale added

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import {FaArrowLeft} from "react-icons/fa6";
 
 const Footer = ({display}) => {
     return (
-        <div className={`${display} hidden border-t border-main-gainsboro bg-main-light-grayish-white mt-14 w-full`}>
+        <div className={`${display} hidden border-t border-main-gainsboro bg-main-light-grayish-white w-full`}>
             <div className="px-4 max-w-[1400px] mx-auto">
                 <div className="pt-14">
                     <div className="grid grid-cols-4">
