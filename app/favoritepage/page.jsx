376be3b7db94b844ef67cc888e4e19ef7565342e
sahmir95 <sahar2200048@gmail.com>
2023-08-10 14:36:32 +0330

    fix(change favorite page)

--- app/favoritepage/page.jsx
+++ app/favoritepage/page.jsx
@@ -2,10 +2,9 @@
 import React, { useState } from "react";
 import Sidbar from "@/components/sidebar/Sidbar";
 import TitlePage from "@/components/titlepage/TiTlePage";
-import { Icon } from "@iconify/react";
-
 import HeaderPages from "@/components/header/headerDesktop/HeaderPages";
-import ButtonFav from "@/components/buttonfav/ButtonFav";
+import HeaderMobilePages from "@/components/header/headerMobile/HeaderMobilePages";
+
 const FavoritePage = ({ data }) => {
   const [favorite, setfavorite] = useState([]);
 
@@ -17,7 +16,7 @@ const FavoritePage = ({ data }) => {
         border="border-b border-b-main-light-gray"
         compFilter=""
       />
-      <ButtonFav />
+      <HeaderMobilePages title="مورد علاقه‌ها"/>
       <div>
         {favorite.length > 0 ? (
           <div className="flex">
@@ -44,16 +43,18 @@ const FavoritePage = ({ data }) => {
             <div className="hidden lg:block w-1/4 mb-10">
               <Sidbar />
             </div>
-            <div className="w-full lg:w-3/4 ">
-              <TitlePage title="مورد علاقه ها" />
-              <div className="flex flex-col items-center justify-center">
+            <div className="w-full lg:w-3/4 h-3/4 ">
+              <div className="hidden lg:block mr-8 ">
+                <TitlePage title="مورد علاقه ها" />
+              </div>
+              <div className="flex flex-col items-center justify-center pt-20 ">
                 <img
                   className="w-3/4 lg:w-1/3 md:w-1/3 mb-4 lg:mr-4"
                   src="https://cdn.jabama.com/original/statics/mobile/img/favorites-empty-state.5eacc43.svg"
                   alt=""
                 />
-                <div className="lg:w-2/3 text-center">
-                  <p className="font-light text-base text-main-slate-gray mt-2">
+                <div className="lg:w-3/3 text-center">
+                  <p className="font-light w-[240px] md:w-[400px] text-base text-main-slate-gray mt-2">
                     لیست اقامتگاه‌های{" "}
                     <strong className="text-main-deep-teal font-bold">
                       مورد علاقه شما
