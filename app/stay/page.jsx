129bd731757a085a199e8d00ab99608ac126c29a
EmadNova <im.nova@yahoo.com>
2023-08-12 21:44:58 +0330

    fix: updated

--- app/stay/page.jsx
+++ app/stay/page.jsx
@@ -1,14 +1,14 @@
 "use client";
 import "../globals.css";
-import { ReadMoreLink } from "@/components/ReadMore/ReadMore";
-import { Facility } from "@/components/facilities/Facility";
+import {ReadMoreLink} from "@/components/ReadMore/ReadMore";
+import {Facility} from "@/components/facilities/Facility";
 import "../../public/images/clock.png";
 import "./page.css";
-import { RulesComponent } from "@/components/rulesComponent/RulesComponent";
-import { CommentsComponent } from "@/components/commentsComponent/CommentsComponent";
+import {RulesComponent} from "@/components/rulesComponent/RulesComponent";
+import {CommentsComponent} from "@/components/commentsComponent/CommentsComponent";
 import SwiperDetailPage from "../../components/swiperDetailPageComponent/SwiperDetailPage";
 import React from "react";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 import ButtonFav from "@/components/buttonfav/ButtonFav";
 import {ImageDetailComponent} from "@/components/imageDetailComponent/ImageDetailComponent";
 import {ReservationBox} from "@/components/ReservationBox/ReservationBox";
@@ -79,7 +79,7 @@ export default function Detail() {
                                     اجاره {data.type} در {data.city} به میزبانی {data.host.name}
                                 </div>
                             </div>
-                                <AvatarCondition data={data}/>
+                            <AvatarCondition data={data}/>
                         </div>
                         <ImmediateReservation data={data}/>
                         <div className="w-full h-[1px] bg-main-light-gray mt-[20px]"></div>
@@ -136,12 +136,12 @@ export default function Detail() {
                         <div className="w-full">
                             <h2 className="mt-[20px] font-bold">توضیحات {data.type}</h2>
                             <div className="w-full mt-[20px]">
-                                <ReadMoreLink text={data.description} type={data.type} />
+                                <ReadMoreLink text={data.description} type={data.type}/>
                             </div>
                         </div>
                         <div className="w-full h-[1px] bg-main-light-gray mt-[20px]"></div>
                         <div className="w-full mt-[20px] flex flex-col justify-start items-start gap-y-[20px]">
-                            <Facility facility={data.facility} type={data.type} />
+                            <Facility facility={data.facility} type={data.type}/>
                         </div>
                         <div className="w-full h-[1px] bg-main-light-gray mt-[20px]"></div>
                         <div className="w-full flex justify-start items-center flex-col">
@@ -163,7 +163,7 @@ export default function Detail() {
                                         میلغ رزرو اضافه می‌شود.
                                     </p>
                                     <p className="font-light text-[0.75rem]">
-                                         قیمت برای هر نفر اضافه به ازای هر شب :
+                                        قیمت برای هر نفر اضافه به ازای هر شب :
                                         <strong className="font-bold text-[0.75rem]">
                                             {data.price.extra} هزار تومان
                                         </strong>
@@ -175,7 +175,8 @@ export default function Detail() {
                         <div className="w-full mt-[20px]">
                             <div className="w-full font-bold">قوانین و مقررات {data.type}</div>
                             <div className="w-full h-[140px] flex justify-between items-center">
-                                <div className="w-[48%] h-[100px] flex justify-around items-start flex-col rounded-[10px] border-[1px] border-solid border-main-light-gray shadow-none p-[10px]">
+                                <div
+                                    className="w-[48%] h-[100px] flex justify-around items-start flex-col rounded-[10px] border-[1px] border-solid border-main-light-gray shadow-none p-[10px]">
                                     <img
                                         className="w-[25px] h-[25px]"
                                         src="/images/clock.png"
@@ -184,7 +185,8 @@ export default function Detail() {
                                     <p className="font-bold text-[1rem]">ساعت ورود</p>
                                     <p className="font-light text-[0.8rem]">02:00 (بعد از ظهر)</p>
                                 </div>
-                                <div className="w-[48%] h-[100px] flex justify-around items-start flex-col rounded-[10px] border-[1px] border-solid border-main-light-gray shadow-none p-[10px]">
+                                <div
+                                    className="w-[48%] h-[100px] flex justify-around items-start flex-col rounded-[10px] border-[1px] border-solid border-main-light-gray shadow-none p-[10px]">
                                     <img
                                         className="w-[25px] h-[25px]"
                                         src="/images/clock.png"
@@ -195,36 +197,38 @@ export default function Detail() {
                                 </div>
                             </div>
                         </div>
-                        <hr className="dashed-2" />
-                        <RulesComponent rules={data.rules} type={data.type} />
+                        <hr className="dashed-2"/>
+                        <RulesComponent rules={data.rules} type={data.type}/>
                         <div className="w-full h-[1px] bg-main-light-gray mt-[8px]"></div>
-                        <CommentsComponent comments={data.reviews} />
-                    <div className="w-full h-[1px] bg-main-light-gray mt-[20px] lg:hidden"></div>
-                    <div className="w-full mt-[20px] flex flex-col px-[20px] lg:hidden">
-                        <div className="font-bold">جستجو {data.type} مشابه</div>
-                        <div className="w-full h-[200px] flex items-start scroll-smooth scroll-p-[20px] gap-x-[20px] mt-[20px] whitespace-nowrap overflow-x-scroll lg:hidden">
-                            <a
-                                href="#"
-                                className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
-                            >
-                                اجاره ویلا و سوئیت در کردان{" "}
-                            </a>
-                            <a
-                                href="#"
-                                className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
-                            >
-                                اجاره ویلا و سوئیت باربیکیودار (منقل) در کردان{" "}
-                            </a>
-                            <a
-                                href="#"
-                                className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
-                            >
-                                اجاره ویلا و سوئیت در استان البرز{" "}
-                            </a>
+                        <CommentsComponent comments={data.reviews}/>
+                        <div className="w-full h-[1px] bg-main-light-gray mt-[20px] lg:hidden"></div>
+                        <div className="w-full mt-[20px] flex flex-col px-[20px] lg:hidden">
+                            <div className="font-bold">جستجو {data.type} مشابه</div>
+                            <div
+                                className="w-full h-[200px] flex items-start scroll-smooth scroll-p-[20px] gap-x-[20px] mt-[20px] whitespace-nowrap overflow-x-scroll lg:hidden">
+                                <a
+                                    href="#"
+                                    className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
+                                >
+                                    اجاره ویلا و سوئیت در کردان{" "}
+                                </a>
+                                <a
+                                    href="#"
+                                    className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
+                                >
+                                    اجاره ویلا و سوئیت باربیکیودار (منقل) در کردان{" "}
+                                </a>
+                                <a
+                                    href="#"
+                                    className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
+                                >
+                                    اجاره ویلا و سوئیت در استان البرز{" "}
+                                </a>
+                            </div>
                         </div>
                     </div>
-                    </div>
-                    <div className="hidden lg:h-full lg:inline lg:sticky lg:top-0 left-0 lg:w-[26%] lg:p-[20px] lg:border-[1px] lg:border-main-light-gray lg:rounded-[10px] mt-[20px] mb-[40px]">
+                    <div
+                        className="hidden lg:h-full lg:inline lg:sticky lg:top-0 left-0 lg:w-[26%] lg:p-[20px] lg:border-[1px] lg:border-main-light-gray lg:rounded-[10px] mt-[20px] mb-[40px]">
                         <ReservationBox data={data}/>
                     </div>
 
@@ -232,96 +236,41 @@ export default function Detail() {
                 <div className="hidden lg:w-full lg:inline lg:h-[1px] lg:bg-main-light-gray lg:mt-[20px]"></div>
                 <div className="hidden lg:w-full lg:mt-[20px] lg:flex lg:flex-col lg:px-[20px]">
                     <div className="font-bold">جستجو {data.type} مشابه</div>
-                <div className="hidden lg:w-full lg:h-[100px] lg:flex lg:items-start lg:gap-x-[20px] lg:mt-[20px]">
-                    <a
-                        href="#"
-                        className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
-                    >
-                        اجاره ویلا و سوئیت در کردان{" "}
-                    </a>
-                    <a
-                        href="#"
-                        className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
-                    >
-                        اجاره ویلا و سوئیت باربیکیودار (منقل) در کردان{" "}
-                    </a>
-                    <a
-                        href="#"
-                        className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
-                    >
-                        اجاره ویلا و سوئیت در استان البرز{" "}
-                    </a>
-                </div>
+                    <div className="hidden lg:w-full lg:h-[100px] lg:flex lg:items-start lg:gap-x-[20px] lg:mt-[20px]">
+                        <a
+                            href="#"
+                            className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
+                        >
+                            اجاره ویلا و سوئیت در کردان{" "}
+                        </a>
+                        <a
+                            href="#"
+                            className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
+                        >
+                            اجاره ویلا و سوئیت باربیکیودار (منقل) در کردان{" "}
+                        </a>
+                        <a
+                            href="#"
+                            className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
+                        >
+                            اجاره ویلا و سوئیت در استان البرز{" "}
+                        </a>
+                    </div>
                 </div>
             </div>
             <div className="w-full flex justify-start items-start flex-col">
-              <p className="font-medium text-[0.9rem]">نفر اضافه</p>
-              <p className="font-light text-[0.75rem]">
-                هزینه‌ای که برای نفرات بیش از استاندارد (سرویس خواب و ... ) به
-                میلغ رزرو اضافه می‌شود.
-              </p>
-              <p className="font-light text-[0.75rem]">
-                قیمت برای هر نفر اضافه به ازای هر شب :
-                <strong className="font-bold text-[0.75rem]">
-                  {data.price.extra} هزار تومان
-                </strong>
-              </p>
+                <p className="font-medium text-[0.9rem]">نفر اضافه</p>
+                <p className="font-light text-[0.75rem]">
+                    هزینه‌ای که برای نفرات بیش از استاندارد (سرویس خواب و ... ) به
+                    میلغ رزرو اضافه می‌شود.
+                </p>
+                <p className="font-light text-[0.75rem]">
+                    قیمت برای هر نفر اضافه به ازای هر شب :
+                    <strong className="font-bold text-[0.75rem]">
+                        {data.price.extra} هزار تومان
+                    </strong>
+                </p>
             </div>
-          </div>
         </div>
-        <div className="w-full h-[1px] bg-main-light-gray mt-[20px]"></div>
-        <div className="w-full mt-[20px]">
-          <div className="w-full font-bold">قوانین و مقررات {data.type}</div>
-          <div className="w-full h-[140px] flex justify-between items-center">
-            <div className="w-[48%] h-[100px] flex justify-around items-start flex-col rounded-[10px] border-[1px] border-solid border-main-light-gray shadow-none p-[10px]">
-              <img
-                className="w-[25px] h-[25px]"
-                src="/images/clock.png"
-                alt="clock"
-              />
-              <p className="font-bold text-[1rem]">ساعت ورود</p>
-              <p className="font-light text-[0.8rem]">02:00 (بعد از ظهر)</p>
-            </div>
-            <div className="w-[48%] h-[100px] flex justify-around items-start flex-col rounded-[10px] border-[1px] border-solid border-main-light-gray shadow-none p-[10px]">
-              <img
-                className="w-[25px] h-[25px]"
-                src="/images/clock.png"
-                alt="clock"
-              />
-              <p className="font-bold text-[1rem]">ساعت خروج</p>
-              <p className="font-light text-[0.8rem]">12:00 (ظهر)</p>
-            </div>
-          </div>
-        </div>
-        <hr className="dashed-2" />
-        <RulesComponent rules={data.rules} type={data.type} />
-        <div className="w-full h-[1px] bg-main-light-gray mt-[8px]"></div>
-        <CommentsComponent comments={data.reviews} />
-      </div>
-      <div className="w-full h-[1px] bg-main-light-gray mt-[20px]"></div>
-      <div className="w-full mt-[20px] flex flex-col px-[20px]">
-        <div className="font-bold">جستجو {data.type} مشابه</div>
-        <div className="w-full h-[200px] flex items-start scroll-smooth scroll-p-[20px] gap-x-[20px] mt-[20px] whitespace-nowrap overflow-x-scroll">
-          <a
-            href="#"
-            className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
-          >
-            اجاره ویلا و سوئیت در کردان{" "}
-          </a>
-          <a
-            href="#"
-            className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
-          >
-            اجاره ویلا و سوئیت باربیکیودار (منقل) در کردان{" "}
-          </a>
-          <a
-            href="#"
-            className="w-fit h-fit bg-main-light-grayish-silver font-medium text-[0.9rem] p-[20px] border-[1px] border-main-light-gray rounded-[10px]"
-          >
-            اجاره ویلا و سوئیت در استان البرز{" "}
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
